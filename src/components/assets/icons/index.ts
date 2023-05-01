4ddf966c57c3bb26d5aba46cec358260b3aa2864
sdf5771 <seobisback@gmail.com>
2023-05-01 18:06:48 +0900

    feat. svg component

--- src/components/assets/icons/index.ts
+++ src/components/assets/icons/index.ts
@@ -1,3 +1,9 @@
+import {ReactComponent as BnBLogo} from './BnB.svg'
+import {ReactComponent as EthereumLogo} from './Ethereum.svg'
+import {ReactComponent as SolanaLogo} from './Solana.svg'
+
 export {
-    
+    BnBLogo, 
+    EthereumLogo, 
+    SolanaLogo,
 }
---
+++ src/components/assets/images/index.ts
@@ -0,0 +1,15 @@
+import {ReactComponent as Close} from './close.svg'
+import {ReactComponent as Info} from './info.svg'
+import {ReactComponent as DownArrow} from './down_arrow.svg'
+import {ReactComponent as BorderDownArrow} from './border_down_arrow.svg'
+import {ReactComponent as SwapArrow} from './swap.svg'
+import {ReactComponent as RightArrow} from './right_arrow.svg'
+
+export {
+    Close,
+    Info,
+    DownArrow,
+    BorderDownArrow,
+    SwapArrow,
+    RightArrow,
+}
