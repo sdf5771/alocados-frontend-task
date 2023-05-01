972176b3d259f63c68662cf091277fc3b7d5550c
sdf5771 <seobisback@gmail.com>
2023-05-01 17:50:26 +0900

    init commit

---
+++ src/Router.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+function Router(){
+    return(
+        <BrowserRouter>
+            <Routes>
+                {/* <Route path='/' element={}/> */}
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router;
---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import Router from './Router'
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Router />
+  </React.StrictMode>
+);
+
+reportWebVitals();
